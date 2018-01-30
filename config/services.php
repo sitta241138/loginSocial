559204c8b4084412c30365cacd1edc62d82e5b54
sitta241138 <sitta241138@hotmail.com>
2018-01-30 15:36:29 +0700

    2 commit

---
+++ config/services.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'facebook' => [
+        'client_id' => '1188905854579433',
+        'client_secret' => '2676a05aa014f9ab5fd0f8c78e47cfca',
+        'redirect' => 'http://localhost:8000/auth/facebook/callback',
+    ],
+
+    'twitter' => [
+        'client_id' => 'RoX6VZrKt2g5GzIAZTmrRdLlV',
+        'client_secret' => 'aVtCnx9gIzAg5ZAcAWjh9rQGN1bWdbNhm5w2xNwGbl0r5AUoSE',
+        'redirect' => 'http://localhost:8000/auth/twitter/callback',
+    ],
+
+];
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/auth/facebook', 'SocialAuthFacebookController@redirect');
+Route::get('/auth/facebook/callback', 'SocialAuthFacebookController@callback');
+
+Route::get("/auth/twitter","SocialAuthTwitterController@redirect");
+Route::get("/auth/twitter/callback", "SocialAuthTwitterController@callback");
